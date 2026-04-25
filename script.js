493062754c2e05f3bbb0f8e7e79a80b1bfc942a6
yashraj-sup <yttfc29062024@email.com>
2026-04-25 15:48:45 +0530

    add theme toggle  and reading time

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+if(document.querySelector('time')){
 const text=document.querySelector('main').innerText;
 const wordcount=text.split(' ').length;
 const readingtime=Math.ceil(wordcount/200);
 document.querySelector('time') .insertAdjacentText('afterend',` - ${readingtime} min read`);
+}
+const toggle=document.querySelector('#theme-toggle');
+toggle.addEventListener('click', function(){
+    document.body.classList.toggle('light-mode');
+    if(document.body.classList.contains('light-mode')){
+        toggle.textContent='🌙';
+    }else{
+        toggle.textContent='☀️';
+    }
+});
